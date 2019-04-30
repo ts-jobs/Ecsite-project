5eb137eda6267da56b2c45dd3eff9e2ec19ef008
shuso1213 <shuso1213@gmail.com>
2019-04-30 11:06:18 +0900

    購入完了画面のマークアップのみ完了

--- complete.php
+++ complete.php
@@ -9,9 +9,9 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/complete.css">
 
-    <title>ログイン(サンプル)</title>
+    <title>購入完了(サンプル)</title>
   </head>
 <body>
 <!-- ここからヘッダー -->
@@ -23,21 +23,15 @@
 <!-- ここまでメイン -->
 <div class="main-wrapper">
  <div class="main-container">
-   <h1 class="main-title">ログイン</h1>
-   <form id="login-form" method="post" action="">
-    <div class="name-area input-field col s12">
-     <label for="name">ユーザー名(半角英数字)：</label>
-     <input type="text" id="name" name="user_name" class="validate">
-    </div>
-    <div class="password-area input-field col s12">
-     <label for="password">パスワード(半角英数字)：</label>
-     <input type="password" id="password" name="user_pass" class="validate">
-    </div>
-    <button class="btn waves-effect waves-light" type="submit" name="action" id="sendBtn">ログイン</button>
-   </form>
+   <h1 class="main-title">ご購入完了</h1>
+   <p class="sub-title">ご購入いただき、ありがとうございます。</p>
+   <p class="complete-txt">購入完了メールを自動送信させて頂いております。商品が到着するまでしばらくお待ち下さい</p>
+   <div class="order-num-area">
+     <p>注文番号：<span id="orderNum">gahiojt8a340j</span></p>
+   </div>
+   <p>商品に関するお問い合わせの際に、注文番号をご利用ください。</p>
+   <p class="btn-area"><a href="" id="backBtn">TopPageへ戻る</a></p>
  </div>
- <p class="forget-pass"><a href="">パスワードをお忘れですか?</a><p>
- <p><a href="" class="waves-effect waves-light btn" id="new-member-btn">新規会員登録はコチラ</a></p>
 </div>
 <!-- ここまでメイン -->
 
