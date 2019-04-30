4dc2c3521289da87047267ad58ad8098e8050d5e
shuso1213 <shuso1213@gmail.com>
2019-04-30 11:23:27 +0900

    会員登録画面ファイルの追加（作業中）

--- new.php
+++ new.php
@@ -9,9 +9,9 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/new.css">
 
-    <title>ログイン(サンプル)</title>
+    <title>会員登録画面(サンプル)</title>
   </head>
 <body>
 <!-- ここからヘッダー -->
