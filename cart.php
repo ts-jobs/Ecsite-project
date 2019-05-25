215790cf65d92012790e9caf11066eaf17381c1e
shuso1213 <shuso1213@gmail.com>
2019-05-25 19:29:07 +0900

    商品カート画面追加（作業途中）

--- cart.php
+++ cart.php
@@ -1,4 +1,4 @@
-<!-- ログイン画面-->
+<!-- 商品カート画面-->
 <!doctype html>
 <html lang="ja">
   <head>
@@ -8,10 +8,10 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="css/cart.css">
 
-    <title>For checking pages.</title>
+    <title>商品カート(サンプル)</title>
   </head>
 <body>
 <!-- ここからヘッダー -->
@@ -21,22 +21,35 @@
 <!-- ここまでヘッダー -->
 
 <!-- ここまでメイン -->
-<div>
-  <a href="./login.php">1.ログイン</a>
-</div>
-<div>
-  <a href="./new.php">2.新規会員登録画面</a>
-</div>
-<div>
-  <a href="./confirm.php">3.登録内容確認画面</a>
-</div>
-<div>
-  <a href="./cart.php">5.商品カート画面</a>
-</div>
-<div>
-  <a href="./complete.php">9.購入完了画面</a>
+<div class="main-wrapper">
+ <div class="main-container">
+   <h1 class="main-title">登録内容確認</h1>
+   <p class="main-txt"><span id="userName">サンプル太郎</span>様のカート商品一覧</p>
+
+   <div class="cart-wrapper">
+
+     <div class="cart-area">
+       <p class="total-area">合計金額：<span id="totalValue">10,000</span>円</p>
+     </div>
+
+     <table id="cartTable">
+       <tr>
+         <th>
+           <div class="left-area">f</div>
+         </th>
+         <td>
+           <div class="right-area">f</div>
+         </td>
+       </tr>
+     </table>
+   </div>
+
+ </div>
 </div>
 <!-- ここまでメイン -->
+
+
+
 
 <!-- ここからフッター -->
 <div class="footer-wrapper">
@@ -50,5 +63,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="js/confirm.js"></script>
 </body>
 </html>
