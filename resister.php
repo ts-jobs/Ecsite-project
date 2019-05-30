c057355a2d7d24cb51d8599d94420fff4b630e76
shuso1213 <shuso1213@gmail.com>
2019-05-31 00:06:05 +0900

    ファイル編集

--- resister.php
+++ resister.php
@@ -22,38 +22,70 @@
 
 <!-- ここまでメイン -->
 <div class="main-wrapper">
-
-
   <div class="main-container1 containers">
-    <h1 class="main-title">カート</h1>
-    <p class="main-txt"><span id="userName">サンプル太郎</span>様のカート商品一覧</p>
-    <div class="cart-wrapper">
-      <table id="cartTable">
-        <tr>
-          <th>
-            <div class="left-area">
-              <img src="images/shohin.png" class="shohins">
-            </div>
-          </th>
-          <td>
-            <div class="right-area">
-             <div class="shohin-areas">
-                <p class="shohin-names">カジュアルなジャケット</p>
-                <p class="shohin-makers">ブランド　：sample makers</p>
-                <p class="shohin-colors">色／サイズ：ブルー／S</p>
-                <p class="shohin-prices">価格　　　：¥5,000-（税込）</p>
-                <p class="shohin-num">個数　　　：1個</p>
-                <p class="delete">削除</p>
-            </div>
-           </div>
-          </td>
-        </tr>
-      </table>
-    </div>
+    <h1 class="main-title">購入確定画面</h1>
+    <table id="cartTable">
+
+      <tr>
+        <th>
+          <div class="left-area">
+            <img src="images/shohin.png" class="shohins">
+          </div>
+        </th>
+        <td>
+          <div class="right-area">
+
+           <div class="shohin-areas">
+              <p class="shohin-names">カジュアルなジャケット</p>
+              <p class="shohin-makers">ブランド　：sample makers</p>
+              <p class="shohin-colors">色／サイズ：ブルー／S</p>
+              <p class="shohin-prices">価格　　　：¥5,000-（税込）</p>
+              <p class="shohin-num">個数　　　：1個</p>
+              <p class="delete">削除</p>
+          </div>
+
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <th>
+          <div class="left-area">
+            <img src="images/shohin.png" class="shohins">
+          </div>
+        </th>
+        <td>
+          <div class="right-area">
+
+           <div class="shohin-areas">
+              <p class="shohin-names">カジュアルなジャケット</p>
+              <p class="shohin-makers">ブランド　：sample makers</p>
+              <p class="shohin-colors">色／サイズ：ブルー／S</p>
+              <p class="shohin-prices">価格　　　：¥5,000-（税込）</p>
+              <p class="shohin-num">個数　　　：1個</p>
+              <p class="delete">削除</p>
+          </div>
+
+          </div>
+        </td>
+      </tr>
+
+    </table>
+
   </div>
 
 
+  <div class="main-container2 containers">
+    <div class="resister-area">
+      <p class="buy-btn-area"><a class="waves-effect waves-light btn" id="buyBtn">購入を確定する</a></p>
+    </div>
 
+    <div class="total-box">
+      <p class="total-value">商品合計　　　<span id="totalValue">10,000</span>円（税込）</p>
+      <p class="send-value">送料　　　　　<span id="sendValue">1,000</span>円</p>
+      <p class="point-value">ポイント利用　<span id="sendValue">1,000</span>円</p>
+    </div>
+
+  </div>
 
 </div>
 <!-- ここまでメイン -->
@@ -73,6 +105,5 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <script src="js/confirm.js"></script>
 </body>
 </html>
