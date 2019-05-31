1ebf945e3c6e18919c0cacfe97d2a395727e883a
shuso1213 <shuso1213@gmail.com>
2019-05-31 22:58:50 +0900

    ファイル編集

--- top.php
+++ top.php
@@ -8,6 +8,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/foundation.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link rel="stylesheet" href="css/top.css">
 
@@ -17,13 +18,6 @@
 <!-- ここからヘッダー -->
 <div class="header-wrapper">
  <p class="logo">ts-jobs株式会社</p>
- <div class="search-area">
-   <input type="search" placeholder="キーワードを入力" id="searchTxt">
-   <a class="waves-effect waves-light btn" id="searchBtn">検索</a>
-   <a href="" id="logIn">ログイン</a>
-   <a href="" id="cart">カート</a>
- </div>
-
 </div>
 <!-- ここまでヘッダー -->
 
@@ -39,7 +33,18 @@
 <div class="main-wrapper">
  <div class="main-container">
     <div class="left-side">left</div>
-    <div class="right-side">right</div>
+    <div class="right-side">
+      <div class="grid-x grid-padding-x small-up-2 medium-up-4 large-up-4">
+        <div class="cell">cell</div>
+        <div class="cell">cell</div>
+        <div class="cell">cell</div>
+        <div class="cell">cell</div>
+        <div class="cell">cell</div>
+        <div class="cell">cell</div>
+        <div class="cell">cell</div>
+        <div class="cell">cell</div>
+      </div>
+    </div>
 
  </div>
 </div>
@@ -58,6 +63,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <script src="js/confirm.js"></script>
+    <script src="js/foundation.min.js"></script>
 </body>
 </html>
