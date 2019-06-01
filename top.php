2e062aa86ae1467c49bc6cb0cc454ae80d18b3ca
shuso1213 <shuso1213@gmail.com>
2019-06-01 17:33:16 +0900

    ファイル編集

--- top.php
+++ top.php
@@ -35,14 +35,112 @@
     <div class="left-side">left</div>
     <div class="right-side">
       <div class="grid-x grid-padding-x small-up-2 medium-up-4 large-up-4">
-        <div class="cell">cell</div>
-        <div class="cell">cell</div>
-        <div class="cell">cell</div>
-        <div class="cell">cell</div>
-        <div class="cell">cell</div>
-        <div class="cell">cell</div>
-        <div class="cell">cell</div>
-        <div class="cell">cell</div>
+
+        <div class="cell">
+          <div class="item-box">
+           <img src="images/wear1.jpg" class="shohins">
+           <p class="shohin-names">カジュアルなジャケット</p>
+           <p class="shohin-makers">sample makers</p>
+           <p class="shohin-prices">¥5,000-（税込）</p>
+           <p class="buy-area">
+             <a href="" class="waves-effect waves-light btn" id="cartBtn">カートへ</a>
+             <a href="" class="waves-effect waves-light btn" id="buyBtn">購入する</a>
+           </p>
+         </div>
+       </div>
+
+       <div class="cell">
+         <div class="item-box">
+          <img src="images/wear2.jpg" class="shohins">
+          <p class="shohin-names">カジュアルなジャケット</p>
+          <p class="shohin-makers">sample makers</p>
+          <p class="shohin-prices">¥5,000-（税込）</p>
+          <p class="buy-area">
+            <a href="" class="waves-effect waves-light btn" id="cartBtn">カートへ</a>
+            <a href="" class="waves-effect waves-light btn" id="buyBtn">購入する</a>
+          </p>
+        </div>
+      </div>
+
+      <div class="cell">
+        <div class="item-box">
+         <img src="images/wear3.jpg" class="shohins">
+         <p class="shohin-names">カジュアルなジャケット</p>
+         <p class="shohin-makers">sample makers</p>
+         <p class="shohin-prices">¥5,000-（税込）</p>
+         <p class="buy-area">
+           <a href="" class="waves-effect waves-light btn" id="cartBtn">カートへ</a>
+           <a href="" class="waves-effect waves-light btn" id="buyBtn">購入する</a>
+         </p>
+       </div>
+     </div>
+
+     <div class="cell">
+       <div class="item-box">
+        <img src="images/wear4.jpg" class="shohins">
+        <p class="shohin-names">カジュアルなジャケット</p>
+        <p class="shohin-makers">sample makers</p>
+        <p class="shohin-prices">¥5,000-（税込）</p>
+        <p class="buy-area">
+          <a href="" class="waves-effect waves-light btn" id="cartBtn">カートへ</a>
+          <a href="" class="waves-effect waves-light btn" id="buyBtn">購入する</a>
+        </p>
+      </div>
+    </div>
+
+    <div class="cell">
+      <div class="item-box">
+       <img src="images/wear1.jpg" class="shohins">
+       <p class="shohin-names">カジュアルなジャケット</p>
+       <p class="shohin-makers">sample makers</p>
+       <p class="shohin-prices">¥5,000-（税込）</p>
+       <p class="buy-area">
+         <a href="" class="waves-effect waves-light btn" id="cartBtn">カートへ</a>
+         <a href="" class="waves-effect waves-light btn" id="buyBtn">購入する</a>
+       </p>
+     </div>
+   </div>
+
+   <div class="cell">
+     <div class="item-box">
+      <img src="images/wear2.jpg" class="shohins">
+      <p class="shohin-names">カジュアルなジャケット</p>
+      <p class="shohin-makers">sample makers</p>
+      <p class="shohin-prices">¥5,000-（税込）</p>
+      <p class="buy-area">
+        <a href="" class="waves-effect waves-light btn" id="cartBtn">カートへ</a>
+        <a href="" class="waves-effect waves-light btn" id="buyBtn">購入する</a>
+      </p>
+    </div>
+  </div>
+
+  <div class="cell">
+    <div class="item-box">
+     <img src="images/wear3.jpg" class="shohins">
+     <p class="shohin-names">カジュアルなジャケット</p>
+     <p class="shohin-makers">sample makers</p>
+     <p class="shohin-prices">¥5,000-（税込）</p>
+     <p class="buy-area">
+       <a href="" class="waves-effect waves-light btn" id="cartBtn">カートへ</a>
+       <a href="" class="waves-effect waves-light btn" id="buyBtn">購入する</a>
+     </p>
+   </div>
+ </div>
+
+ <div class="cell">
+   <div class="item-box">
+    <img src="images/wear4.jpg" class="shohins">
+    <p class="shohin-names">カジュアルなジャケット</p>
+    <p class="shohin-makers">sample makers</p>
+    <p class="shohin-prices">¥5,000-（税込）</p>
+    <p class="buy-area">
+      <a href="" class="waves-effect waves-light btn" id="cartBtn">カートへ</a>
+      <a href="" class="waves-effect waves-light btn" id="buyBtn">購入する</a>
+    </p>
+  </div>
+</div>
+
+
       </div>
     </div>
 
